caf3fe692eb7d53038bd34b311765e914a89ee85
Dknowles <dknowles4489@gmail.com>
2022-02-21 19:36:27 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -32,14 +32,18 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+     
+      // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -58,7 +62,9 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -73,6 +79,14 @@ var fight = function(enemyName) {
       }
     }
   };
+
+  // function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+  
+    return value;
+  };
+
   var startGame = function() {
     
     playerHealth = 100;
@@ -83,7 +97,7 @@ var fight = function(enemyName) {
     if (playerHealth > 0) {
         window.alert("Welcome to Robot Gladiators! " + (i + 1)); 
     var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
     fight(pickedEnemyName);
 
     if (playerHealth > 0 && i < enemyNames.length - 1) {
